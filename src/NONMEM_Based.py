e667170fc11f96729b6a6dea1703c0a033a1554e
AGilsoul <agilsoulphysics@gmail.com>
2023-11-13 17:13:14 -0700

    added percent time above mic

--- src/NONMEM_Based.py
+++ src/NONMEM_Based.py
@@ -7,6 +7,9 @@ ka = 1.02  # first order absorption rate constant
 Vd = 27.7  # drug's volume of distribution in L
 Cl = 21.3  # drug clearance in L/hr
 rate_elim = 0.27  # Cl/Vd
+mic = 1  # mg/mL
+weight = 70  # weight in kg
+mic_mg = mic * weight # mic in mg
 
 
 class TimeStep:
@@ -23,16 +26,32 @@ def d_dt_system(t, IC):
     return [-ka*Agi, ka*Agi - rate_elim * C]
 
 
+def take_dose(hr: int):
+    if hr != 0 and hr % 16 == 0:
+        return False
+    return True
+
+
+def dose_to_dosage(conc, weight):
+    return conc / weight
+
+
+def percent_t_above_mic(conc, weight, dt, total_time):
+    dosage = dose_to_dosage(conc, weight)
+    time_above = sum([1 * dt if x > mic else 0 for x in dosage])
+    return (time_above / total_time) * 100
+
+
 def NONMEM_Based():
     sim_time = 48    # total simulation time in hours
     dose_amt = 1000  # dose amount in milligrams
     dose_interval = 8  # number of hours between doses
     time_steps = []
     for hr in range(sim_time):
-        if hr == 0:
+        if hr == 0 and take_dose(hr):
             print(f'hour 0, dose given')
             IC = [dose_amt, 0]
-        elif hr % dose_interval == 0:
+        elif hr % dose_interval == 0 and take_dose(hr):
             print(f'hour {hr}, dose given')
             IC = [time_steps[-1].Agi + dose_amt, time_steps[-1].C]
         else:
@@ -45,11 +64,15 @@ def NONMEM_Based():
         for i in range(len(eval_points)):
             time_steps.append(TimeStep(eval_points[i], z[0][i], z[1][i]))
 
-    gi_conc = [x.Agi for x in time_steps]
-    ab_conc = [x.C for x in time_steps]
-    t = [x.t for x in time_steps]
+    dt = eval_points[1] - eval_points[0]
+    gi_conc = np.array([x.Agi for x in time_steps])
+    ab_conc = np.array([x.C for x in time_steps])
+    t = np.array([x.t for x in time_steps])
+    mic_r = np.array([mic_mg for x in time_steps])
+    print(percent_t_above_mic(ab_conc, weight, dt, sim_time))
     plt.plot(t, ab_conc, label='AMOX in compartment')
     plt.plot(t, gi_conc, label='AMOX in gi tract')
+    plt.plot(t, mic_r, label='MIC')
     plt.legend()
     plt.show()
     return
